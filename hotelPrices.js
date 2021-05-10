e1e8ecb228e4090ddf94845693affa281717f36e
stela88 <stela88@users.noreply.github.com>
2021-05-10 07:38:01 +0200

    Hotel Prices
    početak

---
+++ hotelPrices.js
@@ -0,0 +1,14 @@
+let pricelist = 
+[
+    {from: "2020-01-01", to: "2020-02-01", price: 34.5},
+    {from: "2020-02-02", to: "2020-03-01", price: 37.0},
+    {from: "2020-03-02", to: "2020-05-15", price: 39.0},
+    {from: "2020-05-16", to: "2020-06-15", price: 37.0},
+];
+
+
+function pricelistFormatter(pricelist){
+
+}
+
+module.exports = priceListFormatter;
