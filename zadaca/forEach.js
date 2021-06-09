09ef6f4da9730cd9662cb279a45b79fa99c0aadc
stela88 <stela88@users.noreply.github.com>
2021-06-09 12:31:09 +0200

    forEach napokon gotov

--- zadaca/forEach.js
+++ zadaca/forEach.js
@@ -1,10 +1,39 @@
-// function letters(letter){
-//    let ranks = letter.forEach(function (e) {
-//        return e + 1;
-//     });
-
-//     return ranks;
-// }
-
-
-// module.exports = {letters}
+let wineData = [
+    {
+        name: 'Malvazija',
+        color: 'gold',
+        temp: '6-8C',
+    },
+    {
+        name: 'Chardonnay',
+        color: 'gold',
+        temp: '6-8C',
+    },
+    {
+        name: 'Rose',
+        color: 'rose',
+        temp: '6-8C',
+    },
+    {
+        name: 'Teran',
+        color: 'red',
+        temp: '16-18C',
+    },
+    {
+        name: 'Merlot',
+        color: 'red',
+        temp: '16-18C',
+    },
+    {
+        name: 'Cabernet Sauvignon',
+        color: 'red',
+        temp: '16-18C',
+    },
+    {
+        name: 'Brut',
+        color: 'gold',
+        temp: '5-7C',
+    },
+];
+  
+  module.exports = { wineData };
--- zadaca/test/forEach.spec.js
+++ zadaca/test/forEach.spec.js
@@ -1,15 +1,115 @@
-// let chai = require("chai");
-// let assert = require('assert');
-// let expect = chai.expect;
-// let should = chai.should;
+let chai = require("chai");
+let assert = require('assert');
+let expect = chai.expect;
+let should = chai.should;
 
-// const {letters} = require("../forEach.js");
+const wine = require('../forEach').wineData;
 
-// describe('forEach', function() {
-    
-//     it('letters', () => {
-//         expect(letters(["a", "b", "c"])).to.be.equal(["a", "b", "c"])
-        
-//     });
-// });
+describe.skip('forEach', function () {
+    it('forEach should return red colored wine', function () {
+      let redWine = [];
+  
+      wine.forEach(function wineColorCheck(wine) {
+        if (wine.color === 'red') {
+          redWine.push(wine.name);
+        }
+      });
+  
+      let expectedOutput = [
+        'Teran',
+        'Merlot',
+        'Cabernet Sauvignon' 
+      ];
+  
+      expect(redWine).to.eql(expectedOutput);
+    });
 
+    it('forEach should return gold colored wine', function () {
+      let goldWine = [];
+  
+      wine.forEach(function wineColorCheck(wine) {
+        if (wine.color === 'gold') {
+          goldWine.push(wine.name);
+        }
+      });
+  
+      let expectedOutput = [
+        'Malvazija',
+        'Chardonnay',
+        'Brut' 
+      ];
+  
+      expect(goldWine).to.eql(expectedOutput);
+    });
+
+
+    it('forEach should return rose colored wine', function () {
+      let roseWine = [];
+  
+      wine.forEach(function wineColorCheck(wine) {
+        if (wine.color === 'rose') {
+          roseWine.push(wine.name);
+        }
+      });
+  
+      let expectedOutput = [
+        'Rose', 
+      ];
+  
+      expect(roseWine).to.eql(expectedOutput);
+    });
+
+    it('forEach should return serving temperature of 6-8C', function () {
+      let firsttempWine = [];
+  
+      wine.forEach(function wineColorCheck(wine) {
+        if (wine.temp === '6-8C') {
+          firsttempWine.push(wine.name);
+        }
+      });
+  
+      let expectedOutput = [
+        'Malvazija',
+        'Chardonnay',
+        'Rose', 
+      ];
+  
+      expect(firsttempWine).to.eql(expectedOutput);
+    });
+
+
+    it('forEach should return serving temperature of 16-18C', function () {
+      let sectempWine = [];
+  
+      wine.forEach(function wineColorCheck(wine) {
+        if (wine.temp === '16-18C') {
+          sectempWine.push(wine.name);
+        }
+      });
+  
+      let expectedOutput = [
+        'Teran',
+        'Merlot',
+        'Cabernet Sauvignon' 
+      ];
+  
+      expect(sectempWine).to.eql(expectedOutput);
+    });
+
+
+    it('forEach should return serving temperature of 5-7C', function () {
+      let thirdtempWine = [];
+  
+      wine.forEach(function wineColorCheck(wine) {
+        if (wine.temp === '5-7C') {
+          thirdtempWine.push(wine.name);
+        }
+      });
+  
+      let expectedOutput = [
+        'Brut'
+      ];
+  
+      expect(thirdtempWine).to.eql(expectedOutput);
+    });
+})
